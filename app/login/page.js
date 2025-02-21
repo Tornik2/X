406ea938effe1549146d5317629e3c69c31611ae
Tornik2 <tornik2@yahoo.com>
2025-02-21 21:41:46 +0400

    fix: make login page inputes controlled

--- app/login/page.js
+++ app/login/page.js
@@ -1,24 +1,68 @@
+"use client"
+
+import { useState } from "react";
 import { Facebook, Apple } from "lucide-react"; // Lucide icons
 import { FcGoogle } from "react-icons/fc"; // Google icon
 import "./login.css";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    try {
+      const response = await fetch("URL", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) throw new Error("Login failed");
+
+      alert("Login successful!");
+      // IF RESPONSE OK ...............
+    } catch (err) {
+      alert(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="login-container">
+    <form onSubmit={handleLogin} className="login-container">
       <h2 className="login-title">Login</h2>
       <p className="login-subtitle">Welcome back! Please log in to access your account.</p>
 <div className="input-row">
       <div className="input-group">
-        <input type="email" placeholder="Enter your Email" />
+      <input
+              type="email"
+              placeholder="Enter your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
       </div>
       <div className="input-group">
-        <input type="password" placeholder="Enter your Password" />
+      <input
+              type="password"
+              placeholder="Enter your Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
       </div>
       </div>
 
       <a href="#" className="forgot-password">Forgot Password?</a>
 
-      <button className="primary-button">Login</button>
+      <button className="primary-button" disabled={loading}>Login</button>
 
       <p className="or-text">Or Continue with</p>
 
@@ -27,6 +71,6 @@ export default function LoginPage() {
         <button className="social-icon"><Facebook size={24} color="#1877F2" /></button>
         <button className="social-icon"><Apple size={24} /></button>
       </div>
-    </div>
+    </form>
   );
 }
