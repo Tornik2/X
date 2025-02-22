200bf68694fcad508ff05b1278eff041a994bec0
Tornik2 <tornik2@yahoo.com>
2025-02-22 17:17:19 +0400

    style themeToggle button

--- app/components/ThemeToggleBtn/ThemeToggleBtn.js
+++ app/components/ThemeToggleBtn/ThemeToggleBtn.js
@@ -1,14 +1,19 @@
 "use client";
-
+import "./ThemeToggleBtn.css";
 import { useTheme } from "../../context/ThemeContext"
+
 
 export default function ThemeToggleBtn() {
   const { theme, toggleTheme } = useTheme();
-
+  const style = {
+    background: theme === "dark" ?  "#fff" : "#000"
+    
+}
   return (
     <button
+    style={style}
       onClick={toggleTheme}
-      className="p-2 rounded bg-gray-200 dark:bg-gray-800 dark:text-white transition"
+      className="p-2 rounded bg-gray-200 dark:bg-gray-800 dark:text-white transition toggleBtn"
     >
       {theme === "light" ? "🌙" : "☀️"}
     </button>
