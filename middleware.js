bcc4c41822b75f0b550ef4b86e0727654a4a5faa
Tornik2 <tornik2@yahoo.com>
2025-02-22 18:50:46 +0400

    fix protected routes

--- middleware.js
+++ middleware.js
@@ -1,20 +1,28 @@
 import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
+import { cookies } from "next/headers"; // 
 
 export async function middleware(req) {
   const path = req.nextUrl.pathname; // Get current path
-  const accessToken = req.cookies.get("accessToken")?.value; // Get accessToken from cookies
+  const cookieStore = cookies(); // Get cookies in middleware
+  const accessToken = await cookieStore.get("accessToken")?.value; 
 
-  const protectedRoutes = ["/merchants", "/leaderboard"];
+  const protectedRoutes = [ "/merchants", "/leaderboard"];
+  const authRoutes = ["/login", "/register"];
 
-  // if unautheticated user tries to access protected routes, redirect to login
+  // If the user is trying to access a protected route but has no accessToken, redirect to login
   if (protectedRoutes.includes(path) && !accessToken) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  return NextResponse.next(); 
+  // If the user is already logged in and tries to access login/register, redirect to homepage
+  if (authRoutes.includes(path) && accessToken) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next(); // Allow access if conditions are met
 }
 
+// protected routes
 export const config = {
-  matcher: ["/merchants", "/leaderboard"], // protect these routes
+  matcher: [ "/merchants", "/leaderboard", "/login", "/register"],
 };
